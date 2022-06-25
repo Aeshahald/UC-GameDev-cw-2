9a81913fa12a4e9191499dbef366bfc21b035197
Aeshah aldosari <108017160+Aeshahald@users.noreply.github.com>
2022-06-25 19:32:23 +0300

    done

---
+++ classwork2.cs
@@ -0,0 +1,38 @@
+﻿
+int Hero = 5;
+int HeroPower = 100;
+int Villian = 10;
+int VillianPower = 200;
+ if (Hero > Villian)
+{
+
+    Console.WriteLine("poor villian");
+
+}
+else
+    Console.WriteLine("poor hero");
+
+
+if (VillianPower > HeroPower)
+
+{
+    Console.WriteLine("Villian");
+
+}
+else
+    Console.WriteLine("Hero");
+
+ double PlayerSpeed = 5.1;
+
+SetSpeed(2.5);
+
+void SetSpeed(double x)
+{
+
+   Console.WriteLine(PlayerSpeed);
+    PlayerSpeed = x;
+    Console.WriteLine(PlayerSpeed);
+
+}
+
+    
